0517717e3f98c8559abbf42a75dbaadb552d0ef0
siva <sivapavan490@gmail.com>
2024-08-30 19:33:28 +0530

    terraform

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -1,3 +1,6 @@
+# by using this teraform file we r fetching the ami-id dynamically .And used that ami for creating the instance
+
+# fetching the ami-id dynamically
 data "aws_ami" "centos-ami" {
   
   owners = ["973714476881"]
@@ -17,4 +20,21 @@ data "aws_ami" "centos-ami" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+}
+# using fetched dynamicaly ami-id crearing ec2-instance.
+resource "aws_instance""web"{
+
+    instance_type= "t2.micro"
+    ami= data.aws_ami.centos-ami.id
+    vpc_security_group_ids = [aws_security_group.roboshop-all.id]
+
+    tags = {
+        Name="pavan"
+    }
+}
+
+# fetching the default-vpc id by using data-sources
+
+data "aws_vpc" "default-vpc-id" {
+  default = true
 }
